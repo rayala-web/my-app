afb5499c984040b74a9379f0481b45c06fa1a1b7
prathusha rayala <prathusharayala@prathushas-MacBook-Pro.local>
2020-01-05 10:24:47 -0600

    initial commit - learning react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,162 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square"> 
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(k) {
+      return <Square value ={k} />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+            
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div></div>
+            <ol></ol>
+          </div>
+        </div>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        
+      );
+    }
+  }
+  
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  */
+ function Button(props) { 
+  const handleClick = () => props.onClickFunction(props.increment);
+	return (
+    <button onClick={handleClick}>
+      +{props.increment}
+    </button>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Display(props) {
+  return ( 
+    <div>
+      {props.message}
+    </div>
+    );
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+  return ( 
+        <div> 
+          <Button onClickFunction={incrementCounter} increment={1} />
+          <Button onClickFunction={incrementCounter} increment={5} />
+          <Button onClickFunction={incrementCounter} increment={10} />
+          <Button onClickFunction={incrementCounter} increment={100} />
+           <Button onClickFunction={incrementCounter} increment={1000} />
+          <Display message={counter}/>
+        </div>
+      );
+}
+ 
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root'),
+);
